4de1001a5fc6644218def20b21dededa47bcb5f7
Fatima Farooq <ff2137@nyu.edu>
2024-04-08 13:06:42 -0400

    completed CMLs, exercise1, and exercise2

--- dv/exercise1.cpp
+++ dv/exercise1.cpp
@@ -1,6 +1,67 @@
-#include <catch2/catch_test_macros.hpp>
 #include <VExercise1.h>
+#include <cstdint>
 
-TEST_CASE("Exercise 1") {
+ // VExercise1 model;
+ 
+
+bool test(uint8_t op)
+{
   VExercise1 model;
+  model.op = op;
+  model.a = 0;
+  model.b = 0;
+  while(++model.b)
+  {
+    while(++model.a)
+    {
+       model.eval();
+       uint8_t result; 
+       if (op == 0)
+       {
+        result = model.a^model.b; 
+       }
+
+       else if (op==1)
+       {
+        result = model.a << model.b; 
+       }
+
+       else if (op==2)
+       {
+        result = model.a % model.b; 
+       }
+
+       else if (op==3)
+       {
+        result = ~(model.a & model.b); 
+       }
+
+       else
+       {
+        return false;
+       }
+      
+      if (model.out != result)
+      {
+        return false;
+      }
+    }
+  }
+
+
+  return true; 
+
 }
+
+int main()
+{
+
+  REQUIRE(test(0) == true);
+  REQUIRE(test(1) == true);
+  REQUIRE(test(2) == true);
+  REQUIRE(test(3) == true);
+
+
+return 0; 
+
+};
--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,6 +1,59 @@
-#include <catch2/catch_test_macros.hpp>
-#include <VExercise2.h>
+[200~#include <VExercise2.h>
+#include <cstdint>
 
-TEST_CASE("Exercise 2") {
-  VExercise2 model;
+uint16_t fib(uint16_t num) {
+  return (num << 1) + (((num >> 10) & 1) ^ ((num >> 8) & 1) ^ ((num >> 3) & 1) ^ ((num >> 1) & 1));
+
+}
+
+bool test(uint16_t num)
+{
+    VExercise2 model; 
+
+  // checking for reset 
+    model.clk = 1;
+    model.eval();
+    model.clk = 0; // getting to negative edge
+    model.reset = 1;
+    model.init = num;
+    model.eval(); // out now has ~ num
+
+    uint16_t last = ~num; 
+
+    for (int i=0; i <100; i++)
+    {
+    // getting negative edge of clock
+    model.clk = 1;
+    model.reset = 0;
+    model.eval();
+    model.clk = 0;
+    model.reset = 0;
+    model.eval();
+
+    // check
+    if (model.out != fib(last)) 
+    {
+      return false;
+    }
+
+    last = model.out; // update last 
+    }
+    
+return true; 
+
+}
+
+int main() {
+
+  REQUIRE(test(5)==true);
+  REQUIRE(test(999)==true);
+  REQUIRE(test(503)==true);
+  REQUIRE(test(1001)==true);
+  REQUIRE(test(5167)==true); 
+  REQUIRE(test(21199)==true);
+  REQUIRE(test(65535)== true);
+
+return 0; 
+
+
 }
